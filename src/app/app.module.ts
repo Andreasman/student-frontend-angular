7eaa2bbaf574778e2dfbef3e08a35c4c5929c500
Andreas Staecker <andreas.staecker@t-online.de>
2018-09-27 15:58:55 +0200

    myLab edit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,15 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {MyLabComponent} from './my-lab/my-lab.component';
+import {StudentListComponent} from './student-list/student-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MyLabComponent,
+    StudentListComponent
   ],
   imports: [
     BrowserModule
